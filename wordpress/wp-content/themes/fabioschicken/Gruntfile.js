a07736c5f3bc8ce429dd41c78c756eb88b270e79
lemonpole <srivera.x1@gmail.com>
2015-06-23 17:17:31 -0400

    converted to grunt tasks

---
+++ wordpress/wp-content/themes/fabioschicken/Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		browserify: {
+			develop: {
+				src: 'js/app.js',
+				dest: 'js/bundle.js',
+				options: {
+					//watch: true,
+					//keepAlive: true,
+					transform: ['reactify'],
+					watchifyOptions: {
+						'poll': true // for NFS mounts (ie: vagrant)
+					}
+				}
+			}
+		},
+		less: {
+			develop: {
+				options: {
+					paths: ['./less'],
+					compress: true
+				},
+				files: {
+					'./bundle.css': './less/main.less'
+				}
+			}
+		},
+		watch: {
+			develop: {
+				files: ["./less/**/*", "./js/**/*"],
+				tasks: ["less:develop", "browserify:develop"]
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-browserify');
+	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('start', ['less:develop','browserify:develop','watch:develop']);
+}
--- wordpress/wp-content/themes/fabioschicken/js/components/App.react.js
+++ wordpress/wp-content/themes/fabioschicken/js/components/App.react.js
@@ -26,7 +26,7 @@ var App = React.createClass({
 	},
 	render: function(){
 		return(
-			<h1>Hello, world...</h1>
+			<h1>Hello, world</h1>
 		);
 	}
 });
