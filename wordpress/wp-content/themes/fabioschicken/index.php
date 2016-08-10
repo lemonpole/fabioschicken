e86e4990e434066b684b671d334a0ba99de095e2
Steve Rivera <srivera@sevone.com>
2016-08-10 09:56:55 -0400

    fix spacing

--- wordpress/wp-content/themes/fabioschicken/index.php
+++ wordpress/wp-content/themes/fabioschicken/index.php
@@ -2,13 +2,13 @@
 
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-	</head>
-	<body>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </head>
+  <body>
     <div id="app"></div>
 
     <script src="<?php echo $root_path; ?>/bundle.js"></script>
-	</body>
+  </body>
 </html>
