f48cb207220fb39913ea5225c56123dd3c54678d
Steve Rivera <srivera@sevone.com>
2016-08-10 17:39:33 -0400

    adds material-ui

--- wordpress/wp-content/themes/fabioschicken/reactapp/app/index.jsx
+++ wordpress/wp-content/themes/fabioschicken/reactapp/app/index.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { render } from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 
-const App = () => (
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+const Sample = () => (
   <Grid>
     <Row>
-      <Col xs={6} md={3}>Hello, World!</Col>
+      <Col xs={6} md={3}>
+        <RaisedButton label="Default" />
+      </Col>
     </Row>
   </Grid>
+);
+
+const App = () => (
+  <MuiThemeProvider>
+    <Sample />
+  </MuiThemeProvider>
 );
 
 render( <App />, document.getElementById( 'app' ) );
