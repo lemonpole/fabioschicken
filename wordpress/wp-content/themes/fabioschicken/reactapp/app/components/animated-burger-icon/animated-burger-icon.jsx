285481e6a24a9e458e511755342e7deebfeb8ee3
Steve Rivera <srivera@sevone.com>
2016-08-11 12:12:38 -0400

    adds other animated burger icon styles

--- wordpress/wp-content/themes/fabioschicken/reactapp/app/components/animated-burger-icon/animated-burger-icon.jsx
+++ wordpress/wp-content/themes/fabioschicken/reactapp/app/components/animated-burger-icon/animated-burger-icon.jsx
@@ -1,27 +1,46 @@
 import React, { Component } from 'react';
 import './animated-burger-icon.scss';
 
+const typeBarNums = {
+  'abi-1': 3,
+  'abi-2': 6,
+  'abi-3': 4,
+  'abi-4': 3
+};
+
 // https://codepen.io/designcouch/pen/Atyop
-// TODO: provide props to specify which animation to use
 export default class AnimatedBurgerIcon extends Component {
   state = {
     toggled: false
+  }
+
+  static defaultProps = {
+    type: 'abi-1'
   }
 
   handleOnClick = () => {
     this.setState({ toggled: !this.state.toggled });
   }
 
+  renderBars = () => {
+    const num = typeBarNums[this.props.type];
+    const output = [];
+
+    for( let i = 0; i < num; i++ ) {
+      output.push( <span /> );
+    }
+
+    return output;
+  }
+
   render() {
     return (
       <div
-        id="animated-burger-icon"
+        id={this.props.type}
         className={this.state.toggled && 'open'}
         onClick={this.handleOnClick}
       >
-        <span />
-        <span />
-        <span />
+        {this.renderBars()}
       </div>
     );
   }
