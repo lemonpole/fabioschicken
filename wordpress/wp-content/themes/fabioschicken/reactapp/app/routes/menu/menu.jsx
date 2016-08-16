30796d8511fcced99043483cf76582dda38b8507
lemonpole <srivera.x1@gmail.com>
2016-08-15 23:30:05 -0400

    adds tabs to menu route

--- wordpress/wp-content/themes/fabioschicken/reactapp/app/routes/menu/menu.jsx
+++ wordpress/wp-content/themes/fabioschicken/reactapp/app/routes/menu/menu.jsx
@@ -1,8 +1,42 @@
 import React from 'react';
+import { Tabs, Tab } from 'material-ui/Tabs';
+import styles from './menu.scss';
+
+const tabStyles = {
+  tabItemContainer: {
+    backgroundColor: '#5e412f'
+  },
+  inkBar: {
+    backgroundColor: 'orange',
+    height: 2
+  },
+  contentContainer: {
+    marginTop: 10
+  }
+};
 
 const Menu = ( props ) => (
-  <section>
-    Hey I'm the menu!
+  <section className={styles.container}>
+    <div className={styles.contentbox}>
+      <Tabs
+        tabItemContainerStyle={tabStyles.tabItemContainer}
+        inkBarStyle={tabStyles.inkBar}
+        contentContainerStyle={tabStyles.contentContainer}
+      >
+        <Tab label="Food Platters">
+          <div />
+        </Tab>
+        <Tab label="Popular Platters">
+          <div />
+        </Tab>
+        <Tab label="Appetizers and Drinks">
+          <div />
+        </Tab>
+        <Tab label="Daily Specials">
+          <div />
+        </Tab>
+      </Tabs>
+    </div>
   </section>
 );
 
