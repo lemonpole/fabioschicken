7b3413df6510e43e138fa79f452b82cdfd335c7f
Steve Rivera <srivera@sevone.com>
2016-09-03 17:26:50 -0400

    adjust reactapp to new json response from wordpress

--- reactapp/app/routes/menu/menu.jsx
+++ reactapp/app/routes/menu/menu.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Row, Col } from 'react-flexbox-grid';
 import { Tabs, Tab } from 'material-ui/Tabs';
 import styles from './menu.scss';
 
@@ -17,40 +16,32 @@ const tabStyles = {
   }
 };
 
-const MenuList = ( props ) => (
-  <Row>
-    <Col xs={12} md={4}>
-      {props.items.map( ( item, id ) => (
-        <Row key={id}>
-          <Col xs={9} md={10}>
-            <span className={styles.lead}>{item.post_title}</span>
-            <p>{item.post_content}</p>
-          </Col>
-          <Col xs={3} md={2}>
-            <code>$13.37</code>
-          </Col>
-        </Row>
-      ) )}
-    </Col>
-  </Row>
-);
-
 class Menu extends Component {
-  renderTabs = () => {
-    const { foods } = this.props;
-    const categories = Object.keys( foods.data );
-    const tabList = [];
+  renderTab = ( category, key ) => {
+    let content;
 
-    categories.map( ( category, id ) => tabList.push(
-      <Tab label={category} key={id}>
-        <MenuList items={foods.data[ category ]} />
+    switch( category ) {
+      case 'Daily Specials':
+        content = ( <div>I AM DAILY SPECIALS</div> );
+        break;
+      case 'Appetizers/Drinks':
+        content = ( <div>I AM THE APPETIZERS</div> );
+        break;
+      default:
+        content = ( <div>I AM EVERYBODY ELSE</div> );
+    }
+
+    return (
+      <Tab label={category} key={key}>
+        {content}
       </Tab>
-    ) );
-
-    return tabList;
+    );
   }
 
   render() {
+    const { foods } = this.props;
+    const categories = Object.keys( foods.data );
+
     return (
       <section className={styles.container}>
         <div className={styles.contentbox}>
@@ -59,7 +50,7 @@ class Menu extends Component {
             inkBarStyle={tabStyles.inkBar}
             contentContainerStyle={tabStyles.contentContainer}
           >
-            {this.renderTabs()}
+            {categories.map( ( category, key ) => this.renderTab( category, key ) )}
           </Tabs>
         </div>
       </section>
