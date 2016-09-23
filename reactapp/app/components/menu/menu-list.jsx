163b282c70a0a8fc83d2ae71ab08348340c501cd
Steve Rivera <srivera.x1@gmail.com>
2016-09-22 23:01:20 -0400

    fixes improper rendering of categories in menu list container

--- reactapp/app/components/menu/menu-list.jsx
+++ reactapp/app/components/menu/menu-list.jsx
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'react-flexbox-grid';
 
-let index = 0;
-
 class MenuList extends Component {
+  index = 0;
+
   renderCols = ( numcols ) => {
     const content = [];
 
-    for( let i = 0; i < numcols; i++, index++ ) {
+    for( let i = 0; i < numcols; i++, this.index++ ) {
       // if we're past the num of children we have let's reset the counter
-      if( index >= this.props.children.length ) {
-        index = 0;
+      if( this.index >= this.props.children.length ) {
+        this.index = 0;
         break;
       }
 
       // otherwise render the column
       content.push(
-        <Col xs={12} md={4} key={i}>{this.props.children[ index ]}</Col>
+        <Col xs={12} md={4} key={i}>{this.props.children[ this.index ]}</Col>
       );
     }
 
@@ -24,14 +24,17 @@ class MenuList extends Component {
   }
 
   render() {
-    const numrows = Math.ceil( this.props.children.length / 3 );
+    // TODO: should numcols be a prop?
     const numcols = 3;
+    const numrows = Math.ceil( this.props.children.length / numcols );
     const content = [];
 
+    // render each row and its title
+    // title should only be rendered on the first iteration
     for( let i = 0; i < numrows; i++ ) {
       content.push(
         <div key={i} style={{ marginBottom: '20px' }}>
-          {this.props.title && ( <h2 style={{ marginBottom: '10px' }}>{this.props.title}</h2> )}
+          {this.props.title && i === 0 && ( <h2 style={{ marginBottom: '10px' }}>{this.props.title}</h2> )}
           <Row>{this.renderCols( numcols )}</Row>
         </div>
       );
