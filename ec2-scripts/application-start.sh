2176d39a95dc2f54c7b3b2f953b147fd16fff648
Steve Rivera <srivera.x1@gmail.com>
2017-05-14 23:02:24 -0400

    add a 30-second delay before attempting to restart the application

--- ec2-scripts/application-start.sh
+++ ec2-scripts/application-start.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+
+# We need to wait 30 secs to give the previous command time to fully shut the tasks down
+sleep 30 && \
 ~/.local/bin/aws ecs update-service --service fabioschicken --desired-count 1 --cluster fabioschicken
